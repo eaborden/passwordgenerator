4643a0a51421b914e9cd95a50f1358d1806b6022
Edward Borden <arroll.borden@gmail.com>
2020-08-18 14:50:29 -0400

    initial submission w js code started

---
+++ Develop/script.js
@@ -0,0 +1,52 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+var numbers = "0123456789"
+var lowercase = "abcdefghijklmnopqrstuvwxyz"
+var uppercase = "QWERTYUIOPLKJHGFDSAZXCVBNM"
+var specialchars = "!@#$%^&*()_+=;:{}[]"
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+function generatePassword() {
+var possibleoptions = ""
+
+// Prompt for length of password
+var passwordLength = prompt ("Enter length of password")
+// Prompt for character types to include in the password
+var wantlowercase = confirm ("Do you want lowercase?")
+var wantuppercase = confirm ("Do you want uppercase?")
+var wantnumbers = confirm ("Do you want numbers")
+var wantspecialchars = confirm ("Do you want special characters")
+
+//If one was not chosen - Alert user must chose one
+if (wantlowercase === false && wantuppercase === false && wantnumbers === false && wantspecialchars === false) 
+return alert ("Must select one.") // will end function
+
+// console.log ("at least one selected")
+
+// Check if one type was selected
+if (wantlowercase === true)
+possibleoptions += lowercase
+if (wantuppercase === true)
+possibleoptions += uppercase
+if (wantnumbers === true)
+possibleoptions += numbers
+if (wantspecialchars === true)
+possibleoptions += specialchars
+
+for (var i =0;i<passwordLength;i++)
+
+//console.log(possibleoptions)
+
+
+}

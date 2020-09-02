ad79be78a3b6896e00d9ad04c34b65a7557b1cf6
Edward Borden <arroll.borden@gmail.com>
2020-09-02 17:16:03 -0400

    Updated js file to include passing password to the generator box on the screen

--- Develop/script.js
+++ Develop/script.js
@@ -18,49 +18,55 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 function generatePassword() {
-var possibleoptions = ""
+  var possibleoptions = ""
 
-// Prompt for length of password
-var passwordLength = prompt ("Enter length of password")
-// Prompt for character types to include in the password
-var wantlowercase = confirm ("Do you want lowercase?")
-var wantuppercase = confirm ("Do you want uppercase?")
-var wantnumbers = confirm ("Do you want numbers")
-var wantspecialchars = confirm ("Do you want special characters")
+  // Prompt for length of password
+  var passwordLength = prompt("Enter length of password")
+  // Prompt for character types to include in the password
+  var wantlowercase = confirm("Do you want lowercase?")
+  var wantuppercase = confirm("Do you want uppercase?")
+  var wantnumbers = confirm("Do you want numbers")
+  var wantspecialchars = confirm("Do you want special characters")
 
-//If one was not chosen - Alert user must chose one
-if (wantlowercase === false && wantuppercase === false && wantnumbers === false && wantspecialchars === false) 
-return alert ("Must select one.") // will end function
+  //If one was not chosen - Alert user must chose one
+  if (wantlowercase === false && wantuppercase === false && wantnumbers === false && wantspecialchars === false)
+    return alert("Must select one.") // will end function
 
-//console.log ("at least one selected")
+  //console.log ("at least one selected")
 
-// Check if one type was selected
-if (wantlowercase === true)
-possibleoptions += lowercase
-if (wantuppercase === true)
-possibleoptions += uppercase
-if (wantnumbers === true)
-possibleoptions += numbers
-if (wantspecialchars === true)
-possibleoptions += specialchars
+  // Check if one type was selected
+  if (wantlowercase === true)
+    possibleoptions += lowercase
+  if (wantuppercase === true)
+    possibleoptions += uppercase
+  if (wantnumbers === true)
+    possibleoptions += numbers
+  if (wantspecialchars === true)
+    possibleoptions += specialchars
 
-//need to get a variable for the 81 characters
-//pass that through a function that get a random index for each character in the array
+  //need to get a variable for the 81 characters
+  //pass that through a function that get a random index for each character in the array
 
 
 
-var password ="";
-for (var i=0; i < passwordLength; i++){
-var selectedoptions = possibleoptions;
+  var password = "";
+  for (var i = 0; i < passwordLength; i++) {
+    var selectedoptions = possibleoptions;
 
-function getRandomIndex(selectedoptions) {
-  return Math.floor(Math.random() * selectedoptions.length);
+    function getRandomIndex(selectedoptions) {
+      return Math.floor(Math.random() * selectedoptions.length);
 
-}
-var onechar = selectedoptions.charAt(getRandomIndex(selectedoptions));
-password += onechar;
-}
-console.log(password);
+    }
+    var onechar = selectedoptions.charAt(getRandomIndex(selectedoptions));
+    password += onechar;
+  }
+  //get password text area by ID
+  //add password into text area
+  // var passwordtext = document.getElementById("password");
+  // passwordtext.value = password;
+
+  console.log(password);
+  return password;
 }
 //assigns the value from the array from the value of the index
 
